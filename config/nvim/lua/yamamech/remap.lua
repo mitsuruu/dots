5ebacf5d9b89ec915450bf91ac7ee03fc92fe756
Yama <30635996+yamamech@users.noreply.github.com>
2023-07-05 23:15:37 +0100

    Simplified remaps and slightly modified nvim-tree and discord configs

--- config/nvim/lua/yamamech/remap.lua
+++ config/nvim/lua/yamamech/remap.lua
@@ -1,24 +1,28 @@
+local bind = vim.keymap.set
+
 vim.g.mapleader = " "
 -- Toggle the NvimTree window
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
+bind("n", "<leader>e", "<cmd>NvimTreeToggle<CR>")
 
 -- Move the highlighted text up/down a line
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+bind("v", "J", ":m '>+1<CR>gv=gv")
+bind("v", "K", ":m '<-2<CR>gv=gv")
 
 -- 
-vim.keymap.set("x", "<leader>p", "\"_dP")
+bind("x", "<leader>p", "\"_dP")
 
 -- 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
-vim.keymap.set("v", "<leader>y", "\"+y")
+bind("n", "<leader>y", "\"+y")
+bind("n", "<leader>Y", "\"+Y")
+bind("v", "<leader>y", "\"+y")
 
 --
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+bind("n", "<leader>d", "\"_d")
+bind("v", "<leader>d", "\"_d")
 
 -- Replace word that the cursor has highlighted in the current file
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+bind("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 -- Add the +x flag to the current file
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+bind("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+bind("n", "<C-s>", "<cmd>w<CR>")
