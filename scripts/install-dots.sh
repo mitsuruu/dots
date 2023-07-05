8ce79c86011f4ffb2d6801dd5bb118cdb2a563f3
Yama <30635996+yamamech@users.noreply.github.com>
2023-07-05 21:40:33 +0100

    Moved scripts and finished writing the install-dots script

---
+++ scripts/install-dots.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+main() {
+	check_vars
+	link_config
+	# link_share # This would need some dotfiles to go in ~/.local/share to be useful
+}
+
+function check_vars {
+	shouldexit=false
+	if [[ -z "$XDG_CONFIG_HOME" ]]; then
+		echo "\$XDG_CONFIG_HOME is not set. Please make sure this is set in your shell."
+		shouldexit=true
+	fi
+	
+	if [[ -z "$XDG_DATA_HOME" ]]; then
+		echo "\$XDG_DATA_HOME is not set. Please make sure this is set in your shell."
+		shouldexit=true
+	fi
+
+	if [ $shouldexit = true ]; then
+		exit 1
+	fi
+}
+
+function link_config {
+	cd "$(dirname "$0")/../config/"
+
+	for i in *
+	do
+		echo "Creating symlink for $PWD/$i in $XDG_CONFIG_HOME/"
+		ln -sf $PWD/$i $XDG_CONFIG_HOME/
+	done
+}
+
+function link_share {
+	cd "$(dirname "$0")/../share/"
+
+	for i in *
+	do
+		echo "Creating symlink for $PWD/$i in $XDG_DATA_HOME/"
+		ln -sf $PWD/$i $XDG_DATA_HOME/
+	done
+}
+
+main "$@"
